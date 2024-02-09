9ff90bac0a832f591d404feb6f0ddb1b5d98b0e6
Nikhil Khadka <khadkanikhil6@gmail.com>
2024-02-09 13:28:06 +0545

    my 1st project

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  
+</head>
+<body>
+<div class="navbar">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+          <a href="#">About Us</a>
+          <a href="#">Rooms</a>
+        </li>
+      </ul>
+      <img src="images/Logo.png" class="logo">
+      <ul>
+        <li>
+          <a href="#">Gallery</a>
+          <a href="#">Facilities</a>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+</body>
+</html>
